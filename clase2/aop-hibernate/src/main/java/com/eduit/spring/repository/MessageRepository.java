7923d9785abfc8da61e7c66b8a5a270fa7956a93
David Salas <dsalasboscan@gmail.com>
2020-03-30 14:13:32 -0300

    remuevo clases aun no dictadas

--- clase2/aop-hibernate/src/main/java/com/eduit/spring/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eduit.spring.repository;
-
-import com.eduit.spring.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Long, Message> {
-}
