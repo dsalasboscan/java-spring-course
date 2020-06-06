ecb97a832b59e032934491aca54a57aeda661639
David Salas <david.salas@globant.com>
2020-06-06 17:45:09 -0300

    implementado factura repository

--- clase3/spring-boot-data/src/main/java/com/eduit/StartupListener.java
+++ clase3/spring-boot-data/src/main/java/com/eduit/StartupListener.java
@@ -1,7 +1,9 @@
 package com.eduit;
 
 import com.eduit.model.Categoria;
+import com.eduit.model.Factura;
 import com.eduit.model.Producto;
+import com.eduit.repository.FacturaRepository;
 import com.eduit.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -14,10 +16,17 @@ public class StartupListener implements ApplicationListener<ContextRefreshedEven
   @Autowired
   private ProductoRepository productoRepository;
 
+  @Autowired
+  private FacturaRepository facturaRepository;
+
   @Override
   public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
     Categoria categoria = new Categoria(null, "utensilios");
     Producto producto = new Producto(null, "tenedor", 2020.0, categoria);
+
+    Factura factura = new Factura();
+
+    facturaRepository.save(factura);
 
     productoRepository.save(producto);
 
