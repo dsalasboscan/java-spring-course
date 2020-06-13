25a81f12cbebfa128a35de9a446250402c7661b9
David Salas <david.salas@globant.com>
2020-06-13 17:06:03 -0300

    clase 4 corte 2

--- clase4/spring-boot-web/src/main/java/com/eduit/controller/ProductoController.java
+++ clase4/spring-boot-web/src/main/java/com/eduit/controller/ProductoController.java
@@ -1,4 +1,32 @@
 package com.eduit.controller;
 
+import com.eduit.model.dto.ProductDto;
+import com.eduit.model.dto.RecursoCreadoDto;
+import com.eduit.service.ProductService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductoController {
+
+  private final ProductService productService;
+
+  public ProductoController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @PostMapping(value = "/producto/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<RecursoCreadoDto> add(@RequestBody ProductDto productDto) {
+    return productService.add(productDto);
+  }
+
+  @GetMapping(value = "/producto/{id}")
+  public ResponseEntity<ProductDto> getById(@PathVariable("id") Long id) {
+    return productService.getById(id);
+  }
 }
--- clase4/spring-boot-web/src/main/java/com/eduit/service/ProductService.java
+++ clase4/spring-boot-web/src/main/java/com/eduit/service/ProductService.java
@@ -1,4 +1,41 @@
 package com.eduit.service;
 
+import com.eduit.model.Producto;
+import com.eduit.model.dto.ProductDto;
+import com.eduit.model.dto.RecursoCreadoDto;
+import com.eduit.repository.ProductoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+  @Autowired
+  private ProductoRepository productoRepository;
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  public ResponseEntity<RecursoCreadoDto> add(ProductDto productDto) {
+    Producto producto = modelMapper.map(productDto, Producto.class);
+    productoRepository.save(producto);
+
+    return new ResponseEntity<>(new RecursoCreadoDto(producto.getId()), HttpStatus.CREATED);
+  }
+
+  public ResponseEntity<ProductDto> getById(Long id) {
+    Optional<Producto> producto = productoRepository.findById(id);
+
+    if (producto.isPresent()) {
+      ProductDto productDto = modelMapper.map(producto.get(), ProductDto.class);
+      return new ResponseEntity<>(productDto, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+  }
 }
